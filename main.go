cb4d86d52cad333a731d2b287425fb8aa7f304ed
daqige <18863662628@163.com>
2022-03-22 21:07:24 +0800

    test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	c, _, err := zk.Connect([]string{"10.109.0.15"}, time.Second) //*10)
+	c, _, err := zk.Connect([]string{"10.109.58.101"}, time.Second) //*10)
 	if err != nil {
 		panic(err)
 	}
@@ -20,8 +20,9 @@ func main() {
 	e := <-ch
 	fmt.Printf("%+v\n", e)
 	servers := make([]string, 0)
-	servers = append(servers, "server.4=zk-cluster-8nxyny-3.zk-cluster-8nxyny-headless.zk-cluster.svc.cluster5.nbj04.corp.yodao.com:2888:3888:participant;0.0.0.0:2181")
-	reconfig, err := c.Reconfig(servers, -1)
+	servers = append(servers, "server.1=zk-cluster-5a914f-0.zk-cluster-5a914f-headless.zk-cluster.svc.cluster5.nbj04.corp.yodao.com:2888:3888:participant;0.0.0.0:2181")
+
+	reconfig, err := c.IncrementalReconfig(servers, nil, -1)
 	if err != nil {
 		log.Fatalln(err)
 	}
