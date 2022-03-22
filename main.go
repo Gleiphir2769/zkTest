f952aef72482f85602701a53d7b9f1fedc99a1b0
daqige <18863662628@163.com>
2022-03-22 21:50:05 +0800

    test

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/samuel/go-zookeeper/zk"
-	"log"
+	"strconv"
 	"time"
 )
 
@@ -19,13 +19,37 @@ func main() {
 	//fmt.Printf("%+v %+v\n", children, stat)
 	//e := <-ch
 	//fmt.Printf("%+v\n", e)
-	servers := make([]string, 0)
-	servers = append(servers, "1")
-
-	reconfig, err := c.IncrementalReconfig(nil, servers, -1)
-	if err != nil {
-		log.Fatalln(err)
+	server := &Server{
+		id:   1,
+		addr: "zk-cluster-5a914f-0.zk-cluster-5a914f-headless.zk-cluster.svc.cluster5.nbj04.corp.yodao.com:2888:3888:participant;0.0.0.0:2181",
 	}
+
+	err = AddServer(c, server)
 	fmt.Println("-----------------")
-	fmt.Println(reconfig)
+	if err != nil {
+		fmt.Println("operation success")
+	}
+}
+
+type Server struct {
+	id   int
+	addr string
+}
+
+func (s Server) String() string {
+	return fmt.Sprintf("server.%d=%s", s.id, s.addr)
+}
+
+func AddServer(conn *zk.Conn, server *Server) error {
+	servers := make([]string, 1)
+	servers[0] = server.String()
+	_, err := conn.IncrementalReconfig(servers, nil, -1)
+	return err
+}
+
+func RemoveServer(conn *zk.Conn, server *Server) error {
+	servers := make([]string, 1)
+	servers[0] = strconv.Itoa(server.id)
+	_, err := conn.IncrementalReconfig(nil, servers, -1)
+	return err
 }
